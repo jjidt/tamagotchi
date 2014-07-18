156ededbd4cad5e06e743fa342a0c199f5e1df0e
J.J. Idt <johnidt@gmail.com>
2014-07-17 20:56:06 -0700

    added variable timer and text box to add food, sleep, or activity points by typing commands

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,66 @@ var Tamagotchi = {initialize: function(inputName){
                     this.activityLevel = 10;
                     },
                   isAlive: function(){
-                    return this.foodLevel > 0;
+                    return this.foodLevel > 0 && this.sleepLevel > 0 && this.activityLevel > 0;
                   },
                   timePasses: function(){
                     this.foodLevel -= 1;
+                    this.sleepLevel -= 1;
+                    this.activityLevel -= 1;
                   },
 
-}
+};
 
+var speed = 700;
+
+$(document).ready(function(){
+  var command;
+
+
+  var showLevels = function(){
+    $("#food").text(myToma.foodLevel)
+    $("#sleep").text(myToma.sleepLevel)
+    $("#activity").text(myToma.activityLevel)
+  };
+
+  var myToma = Object.create(Tamagotchi);
+  myToma.initialize("steve");
+  $(document).click(function(){
+    $("#text-input").focus();
+  });
+
+  showLevels();
+
+  $("#text-input").focus();
+
+  $("#user-type").submit(function(event){
+    event.preventDefault();
+    command = $("#text-input").val();
+    if(command === "eat") {
+      myToma.foodLevel += 10;
+    } else if(command === "sleep") {
+      myToma.sleepLevel += 10;
+    } else if(command === "run") {
+      myToma.activityLevel += 10;
+    }
+    $("#text-input").val("");
+    showLevels();
+  });
+
+  var interval = function(){
+    myToma.foodLevel -= 1;
+    myToma.sleepLevel -= 1;
+    myToma.activityLevel -= 1;
+    showLevels();
+    if(speed > 100) {
+      speed -= 4;
+    };
+    console.log(speed);
+  };
+  var timer;
+  (function repeat() {
+    interval();
+    timer = setTimeout(repeat, speed);
+  })();
+
+});
